ea1444ad109a46fb867db2a52a036e4b21005966
Victorio <victoriocaccioppoli@gmail.com>
2023-10-04 21:20:08 -0300

    E3 - JAVASCRIPT

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const pizzas = [
     nombre: "pizza de Muzzarella",
     precio: 500,
     ingredientes: ["Muzzarella", "Tomate", "Aceitunas"],
+    imagen: "./img/muzzarella.png",
   },
 
   {
@@ -11,17 +12,11 @@ const pizzas = [
     nombre: "pizza de Cebolla",
     precio: 1500,
     ingredientes: ["Muzzarella", "Tomate", "Cebolla"],
+    imagen: "./img/cebolla.png",
   },
 
   {
     id: 3,
-    nombre: "pizza Napolitana",
-    precio: 1350,
-    ingredientes: ["Muzzarella", "Tomate", "Aceitunas", "Anchoas"],
-  },
-
-  {
-    id: 4,
     nombre: "pizza 4 Quesos",
     precio: 1380,
     ingredientes: [
@@ -31,46 +26,81 @@ const pizzas = [
       "Parmesano",
       "Roquefort",
     ],
+    imagen: "./img/4quesos.png",
+  },
+
+  {
+    id: 4,
+    nombre: "pizza Especial",
+    precio: 1000,
+    ingredientes: ["Muzzarella", "Tomate", "Rucula", "Jamón"],
+    imagen: "./img/especial.png",
   },
 
   {
     id: 5,
-    nombre: "pizza Especial",
-    precio: 1000,
-    ingredientes: ["Muzzarella", "Tomate", "Aceitunas", "Anchoas", "Cebolla"],
-  },
-  {
-    id: 6,
     nombre: "pizza con Anana",
     precio: 600,
     ingredientes: ["Muzzarella", "Tomate", "Anana"],
+    imagen: "./img/anana.png",
   },
 ];
 
-console.log('')
+const inputNumber = document.getElementById("inputNumber");
+const pizzaContainer = document.querySelector(".showingPizza");
 
-const pizzaPairId = (() => {
-  const pizzaPairId = pizzas.filter((pizza) => pizza.id % 2 === 0);
-  console.log("Pizzas con ID par:", pizzaPairId);
-})();
+let maxNumber = 5; // Numero máximo a ingresar
+let minNumber = 1; // Numero minimo a ingresar
 
-console.log('')
+// pizzaContainer.innerHTML = myStorage
 
-const lowCostPizza = (() => {
-  const lowCostPizza = pizzas.filter((pizza) => pizza.precio < 600);
-  console.log("Pizzas con precio menor a 600:", lowCostPizza);
-})();
+const showPizza = (id) => {
+  const isValid = id <= maxNumber && id >= minNumber;
+  if (id === " ") {
+    pizzaContainer.innerHTML = `<small class="errorText">Introduce un número válido</small>`;
+  } else if (isValid) {
+    const pizza = pizzas.find((pizza) => pizza.id === Number(id));
 
-console.log("");
+    let element = `
+    <h1>${pizza.nombre}</h1>
+    <img src="${pizza.imagen}" alt=${pizza.nombre}"/>
+    <h2>$${pizza.precio}</h2>
+    `;
 
-const pizzaInfo = (() => {
-  const info = pizzas.map((pizza) => console.log(pizza.nombre, pizza.precio));
-})();
+    pizzaContainer.style.background = `linear-gradient(50deg, rgba(0,0,0,0.85), rgba(0,0,0,0.85)),url(${pizza.imagen})`;
+    pizzaContainer.innerHTML = element;
+    localStorage.setItem("lastPizza", JSON.stringify(pizza));
+  } else {
+    pizzaContainer.style.background = "none";
+    pizzaContainer.innerHTML = `<small class="errorText">Introduce un número válido</small>`;
+  }
+};
 
-const functionIngredientes = (()=>{
-  const ingredientes = pizzas.map((pizza) => {
-    console.log(`La pizza ${pizza.nombre} tiene los ingredientes:`)
-    pizza.ingredientes.map((ingrediente) => console.log(ingrediente))
-    console.log('')
+const readNumber = () => {
+  addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    showPizza(inputNumber.value);
   });
-})()
+};
+
+// Funcion inicializadora, y verifica si ya hay una pizza en el storage
+function init() {
+  const myStorage = JSON.parse(localStorage.getItem("lastPizza")) || "";
+  if (myStorage === "")pizzaContainer.innerHTML = '<small class="errorText">Tenés que ingresar un numero</small>'
+
+  if (myStorage != "") {
+
+    pizzaContainer.innerHTML = `
+    <h1>${myStorage.nombre}</h1>
+    <img src="${myStorage.imagen}" alt=${myStorage.nombre}"/>
+    <h2>$${myStorage.precio}</h2>
+    `;
+
+    pizzaContainer.style.background = `linear-gradient(50deg, rgba(0,0,0,0.85), rgba(0,0,0,0.85)),url(${myStorage.imagen})`;
+  }
+
+  readNumber();
+}
+
+init();
